0f4704904f092c3ef0f3e11e8e9bab526671cfec
sarvesh <sarvesh@gmail.com>
2019-01-05 15:31:54 +0530

    Added OTP functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask,session
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
+import pyotp
+import socket
 
 pymysql.install_as_MySQLdb()
+
+totp = pyotp.TOTP("JBSWY3DPEHPK3PXP",interval=10)
 
 app = Flask(__name__)
 
@@ -11,3 +15,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:sarvesh@localhost/
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+HOST = "192.168.43.223"
+PORT = 6000
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((HOST, PORT))
+
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-from app import app
-
-from flask import render_template
+from app import app,totp,sock
+from app.models import Aadhaar
+from flask import render_template,session
 
 @app.route("/")
 def index():
@@ -10,6 +10,24 @@ def index():
 def getUsage():
 	return render_template("getUsage.html")
 
+@app.route("/sendOtp")
+def sendOtp():
+	session['currentUser'] = "592036400388"
+	beneficiary = Aadhaar.query.get(session['currentUser'])
+
+	otp = totp.now()
+	session['OTP'] = otp
+	string = "{phone_no:"+beneficiary.mobileNo+",OTP:"+otp+"}"
+	stringToSend = ((string)+"\n")
+	sock.send(stringToSend.encode())
+	return "True"
+
+@app.route("/verifyOtp/<string:otp>")
+def verifyOtp(otp):
+	if session['OTP'] == otp:
+		return "True"
+	else:
+		return "False"
 
 if __name__ == "__main__":
 	app.run(host="localhost",port=8000,debug=True)
