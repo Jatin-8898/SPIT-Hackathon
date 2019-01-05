e598f8357cd7ec89820e8c2b197debc81a1bbfa4
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-01-05 19:59:04 +0530

    Added route

--- run.py
+++ run.py
@@ -2,15 +2,16 @@ from app import app
 
 from flask import render_template
 
+
 @app.route("/")
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
+
 
 @app.route("/stock")
-def index():
-	return render_template("stock.html")
+def stock():
+    return render_template("stock.html")
 
 
 if __name__ == "__main__":
-	app.run(host="localhost",port=8000,debug=True)
-
+    app.run(host="localhost", port=8000, debug=True)
